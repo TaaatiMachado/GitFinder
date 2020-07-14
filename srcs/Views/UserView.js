596e7366f957812f4232f36748839098225c503e
Tati Machado <taaatimachado@gmail.com>
2020-07-14 15:56:35 -0300

    Projeto finalizado

--- srcs/Views/UserView.js
+++ srcs/Views/UserView.js
@@ -2,7 +2,8 @@ class UserView {
 
     static template(user) {
         return `<div id="profile">
-                    <img id="imgProfile" src="${user.avatar}">
+                    <a href="https://github.com/${inputSearch.value}">
+                    <img id="imgProfile" src="${user.avatar}"> </a>
                     <div>
                     <p> Login: ${user.login}</p>
                     <p> Name: ${user.name}</p>
